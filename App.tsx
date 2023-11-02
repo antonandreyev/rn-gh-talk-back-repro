a1629a176042802f7def2c0b1b909a8fc4b4a808
Anton Andreev <5842508+antonandreyev@users.noreply.github.com>
2023-11-02 12:20:46 +0200

    init

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+
+function App() {
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Text>text 1</Text>
+      <Text>text 2</Text>
+      <Text>text 3</Text>
+      <Text>text 4</Text>
+      <Text>text 5</Text>
+      <Text>text 6</Text>
+      <Text>text 7</Text>
+      <Text>text 8</Text>
+      <Text>text 9</Text>
+      <Text>text 10</Text>
+      <Text>text 11</Text>
+      <Text>text 12</Text>
+      <Text>text 13</Text>
+      <Text>text 14</Text>
+      <Text>text 15</Text>
+    </GestureHandlerRootView>
+  );
+}
+
+export default App;
